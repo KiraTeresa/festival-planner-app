5c10836d81167b3be9c5685cc64b82f50c1e8646
Kira Soba <kira.soba@gmx.de>
2022-08-13 20:19:32 +0200

    Add: views/group/all

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ app.use("/auth", authRoutes);
 const userRoutes = require("./routes/user.routes");
 app.use("/user", userRoutes);
 
+const groupRoutes = require("./routes/group.routes");
+app.use("/group", groupRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
