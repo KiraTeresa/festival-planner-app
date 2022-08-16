6f040318e528514f5df339f2244e3fe8f54cfef2
Kira Soba <kira.soba@gmx.de>
2022-08-16 21:19:01 +0200

    Add: display artist specific details through spotifyApi

--- routes/band.routes.js
+++ routes/band.routes.js
@@ -20,6 +20,7 @@ router.get("/search-result", (req, res) => {
     );
 });
 
+// band details:
 router.get("/:bandName/:festivalID", (req, res) => {
   const band = req.params.bandName;
   const festivalID = req.params.festivalID;
@@ -28,8 +29,24 @@ router.get("/:bandName/:festivalID", (req, res) => {
       const bandFound = festival.bands.find(
         (element) => element.bandName === band
       );
-      const { bandName, stage, day, startTime, endTime } = bandFound;
-      res.render("band/details", { bandName, stage, day, startTime, endTime });
+      const { bandName, spotifyId, stage, day, startTime, endTime } = bandFound;
+      spotifyApi.getArtist(spotifyId).then((artist) => {
+        const { genres, images } = artist.body;
+        spotifyApi.getArtistTopTracks(spotifyId, "de").then((topTracks) => {
+          const { tracks } = topTracks.body;
+          console.log("Found the top tracks: ", tracks);
+          res.render("band/details", {
+            bandName,
+            stage,
+            day,
+            startTime,
+            endTime,
+            genres,
+            images,
+            tracks,
+          });
+        });
+      });
     })
     .catch((err) =>
       console.log("Did not find the band you were looking for", err)
