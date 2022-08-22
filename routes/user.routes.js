8484a65b084df100833aa5283a17014b398aca40
Kira Soba <kira.soba@gmx.de>
2022-08-22 18:37:33 +0200

    Update: show personal watchlist on user dashboard

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,8 +6,9 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 router.get("/dashboard", isLoggedIn, (req, res) => {
   User.findById(req.session.user)
     .then((user) => {
-      const { username, notifications } = user;
-      res.render("user/dashboard", { username, notifications });
+      const { username, watchlist, notifications } = user;
+
+      res.render("user/dashboard", { username, notifications, watchlist });
     })
     .catch((err) => console.log("Something went wrong", err));
 });
