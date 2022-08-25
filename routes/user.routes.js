99127ab6cb5fe208d19fa4dfe71971ff978e32f2
Kira Soba <kira.soba@gmx.de>
2022-08-25 19:43:32 +0200

    Fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,21 +4,25 @@ const User = require("../models/User.model");
 const Group = require("../models/Group.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-router.get("/dashboard", isLoggedIn, (req, res) => {
+router.get("/dashboard", isLoggedIn, async (req, res) => {
   User.findById(req.session.user)
     .populate("groups")
     .then(async (user) => {
       const { username, watchlist, notifications, groups } = user;
 
       let usersGroups = [];
+      console.log("GROUPS BEFORE LOOP: ", usersGroups);
       if (groups) {
-        groups.forEach(async (element) => {
+        for (const element of groups) {
           await Group.findById(element.toString()).then(async (group) => {
             const { groupName } = group;
             usersGroups.push(groupName);
+            console.log("Element: ", element);
+            console.log("-------");
           });
-        });
+        }
       }
+      console.log("USERS GROUPS AFTER LOOP: ", usersGroups);
       res.render("user/dashboard", {
         username,
         notifications,
